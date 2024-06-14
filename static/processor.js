38ac6f88b77502d91c7750cac3f1400c7267604d
Adil ABBADI <adil.abbadi.1996@gmail.com>
2024-06-15 00:30:53 +0100

    siren: remove 0 from audio buffer

--- static/processor.js
+++ static/processor.js
@@ -3,20 +3,28 @@ class AudioProcessor extends AudioWorkletProcessor {
         super();
         this.audioDataBuffer = [];
         this.sampleRate = 22050;  // Specify the sample rate
+        this.framePerBuffer = 500 ;
+        this.int16Array = [];
     }
 
     process(inputs) {
         const input = inputs[0];
         if (input.length > 0) {
             const inputBuffer = input[0];
-            const int16Array = new Int16Array(inputBuffer.length);
+           
 
             for (let i = 0; i < inputBuffer.length; i++) {
                 const s = Math.max(-1, Math.min(1, inputBuffer[i])); // Clamp the value to [-1, 1]
-                int16Array[i] = s < 0 ? s * 32768 : s * 32767; // Convert to int16
+                if (s !== 0) {
+                    this.int16Array.push(s < 0 ? s * 32768 : s * 32767);  // Convert to int16
+                }
             }
 
-            this.audioDataBuffer.push(...int16Array);
+            if(this.int16Array.length >= this.framePerBuffer){
+                this.audioDataBuffer.push(...this.int16Array);
+                this.int16Array = [];
+            }
+           
 
             if (this.audioDataBuffer.length >= this.sampleRate * 3) {  // Send every 3 seconds
                 this.port.postMessage(new Int16Array(this.audioDataBuffer).buffer);
